ccb0e2fd9f0f3f72631581fea9d376d62ddf01e8
woodstock13 <goutybattlenet@gmail.com>
2019-12-17 21:44:24 +0100

    base caroussel ok

--- src/components/carouselComponent.js
+++ src/components/carouselComponent.js
@@ -11,35 +11,96 @@ function CarousselComponent(props) {
   }
 
   let carroussel = (
-    <Carousel activeIndex={index} onSelect={handleSelect}>
+    <Carousel activeIndex={index} onSelect={handleSelect} controls={false}>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/430x140.png"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
+        <Row>
+          <Col>
+            <img
+              style={{ background: "white", width: "330px", height: "450px" }}
+              src="https://via.placeholder.com/430x640.png"
+              alt="Second slide"
+            />
+          </Col>
+          <Col>
+            <div
+              style={{
+                textAlign: "left",
+                position: "relative",
+                margin: "0 auto",
+                clear: "left",
+                height: "auto",
+                z_index: "0",
+                text_align: "center",
+              }}
+            >
+              <h3>First slide label</h3>
+              <p>
+                Un peu touche a tout niveau sport, seul le CrossFit a changé ma
+                vie en 2013. A tel point que j'ai décider d'aider les gens à
+                améliorer leur santé et leur vie plus largement. Alias le
+                guerriseur, pour moi la santé passe avant la performance, mais
+                l'envie de se dépasser au sein du groupe, de la communauté reste
+                primordial. La virtuosité avant l'intensité, partage, discipline
+                et dépassement de soi, entraide et plaisir sont les valeurs qui
+                raisonnent en moi quand je parle de CrossFit. Mon rôle principal
+                et d'être head coach, je m'occupe de la programmation de la box,
+                de coordonner la trame des coachs et assistants coach. Je reste
+                avant tout un coach à part entière. Avec mon associé Mathieu
+                Nunes nous nous employons le reste du temps au développement et
+                la pérennité des box d'Aix en Provence dans le 13 et de Blanzy
+                dans le 71. .
+              </p>
+            </div>
+          </Col>
+        </Row>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/430x140.png"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
+        <Row>
+          <Col>
+            <div
+              style={{ background: "white", width: "330px", height: "450px" }}
+            >
+              <img
+                className="d-block w-100"
+                src="https://via.placeholder.com/430x640.png"
+                alt="Second slide"
+              />
+            </div>
+          </Col>
+          <Col>
+            <div
+              style={{
+                position: "relative",
+                margin: "0 auto",
+                clear: "left",
+                height: "auto",
+                z_index: "0",
+                text_align: "center",
+              }}
+            >
+              <h3>First slide label</h3>
+              <p>
+                Un peu touche a tout niveau sport, seul le CrossFit a changé ma
+                vie en 2013. A tel point que j'ai décider d'aider les gens à
+                améliorer leur santé et leur vie plus largement. Alias le
+                guerriseur, pour moi la santé passe avant la performance, mais
+                l'envie de se dépasser au sein du groupe, de la communauté reste
+                primordial. La virtuosité avant l'intensité, partage, discipline
+                et dépassement de soi, entraide et plaisir sont les valeurs qui
+                raisonnent en moi quand je parle de CrossFit. Mon rôle principal
+                et d'être head coach, je m'occupe de la programmation de la box,
+                de coordonner la trame des coachs et assistants coach. Je reste
+                avant tout un coach à part entière. Avec mon associé Mathieu
+                Nunes nous nous employons le reste du temps au développement et
+                la pérennité des box d'Aix en Provence dans le 13 et de Blanzy
+                dans le 71. .
+              </p>
+            </div>
+          </Col>
+        </Row>
       </Carousel.Item>
     </Carousel>
   )
-
-  //   let coachComponent = (
-
-  //   )
 
   return (
     <Row>
@@ -47,4 +108,5 @@ function CarousselComponent(props) {
     </Row>
   )
 }
+
 export default CarousselComponent
